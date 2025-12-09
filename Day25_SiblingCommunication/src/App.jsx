68de9a7a2b3a0208866ae1943fea630bc21c7078
Ozodbek <bakhriddinoff01@gmail.com>
2025-12-09 22:28:03 +0500

    modern design

--- Day25_SiblingCommunication/src/App.jsx
+++ Day25_SiblingCommunication/src/App.jsx
@@ -7,7 +7,7 @@ export default function App() {
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-8">
-      <h1 className="text-3xl font-bold mb-6">Sibling Communication (React)</h1>
+      <h1 className="text-3xl font-bold mb-6">Day 25 Sibling Communication (React)</h1>
 
       <Childa setMessage={setMessage} />
       <Childb message={message} />
