deac1b84f9fb7592ce7f44fdc84fbc2fea2ebbed
Ozodbek <bakhriddinoff01@gmail.com>
2025-12-09 22:32:03 +0500

    bigger messaging content

--- Day25_SiblingCommunication/src/components/Childa.jsx
+++ Day25_SiblingCommunication/src/components/Childa.jsx
@@ -1,6 +1,6 @@
 export default function ChildA({ setMessage }) {
   return (
-    <div className="bg-white p-4 rounded-xl shadow w-80 mb-4">
+    <div className="bg-white p-4 rounded-xl w-[500px] shadow w-80 mb-4">
       <h2 className="text-xl font-semibold mb-2">Child A</h2>
       <input
         type="text"
--- Day25_SiblingCommunication/src/components/Childb.jsx
+++ Day25_SiblingCommunication/src/components/Childb.jsx
@@ -1,6 +1,6 @@
 export default function ChildB({ message }) {
   return (
-    <div className="bg-white p-4 rounded-xl shadow w-80">
+    <div className="bg-white p-4 w-[500px] rounded-xl shadow w-80">
       <h2 className="text-xl font-semibold mb-2">Child B</h2>
       <p className="text-gray-700">Child A dan kelgan xabar:  
         <span className="font-bold"> {message || "Hali xabar yo'q"} </span>
