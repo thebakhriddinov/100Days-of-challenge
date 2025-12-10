c97e435733f9a491b51c20c8bcfd1ce3ee677308
Ozodbek <bakhriddinoff01@gmail.com>
2025-12-10 22:35:23 +0500

    change style

--- Day26_CountdownTimer/src/App.jsx
+++ Day26_CountdownTimer/src/App.jsx
@@ -2,7 +2,7 @@ import Countdown from "./Countdown";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-indigo-200 to-sky-200 flex items-center justify-center p-6">
+    <div className="min-h-screen bg-[#030732] flex items-center justify-center p-6">
       <Countdown />
     </div>
   );
