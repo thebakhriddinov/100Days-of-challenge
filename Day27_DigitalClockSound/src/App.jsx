ed40a5e9bf90ede68783e1f98223e5073f40d898
Ozodbek <bakhriddinoff01@gmail.com>
2025-12-12 19:54:41 +0500

    initial commit

---
+++ Day27_DigitalClockSound/src/App.jsx
@@ -0,0 +1,10 @@
+import {react} from 'react';
+import Clock from "./Clock"
+
+export default function App() {
+  return (
+    <div>
+      <Clock/>
+    </div>
+  )
+}
---
+++ Day27_DigitalClockSound/src/Clock.jsx
@@ -0,0 +1,7 @@
+export default function Clock() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
