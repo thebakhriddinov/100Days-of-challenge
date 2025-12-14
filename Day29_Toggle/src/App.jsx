b2f8a4114f9b355057d0d3c78a1eb2508ea220cc
Ozodbek <bakhriddinoff01@gmail.com>
2025-12-14 18:19:42 +0500

    add function

--- Day29_Toggle/src/App.jsx
+++ Day29_Toggle/src/App.jsx
@@ -5,15 +5,18 @@ export default function App() {
   
   return (
     <div className="h-screen flex flex-col items-center justify-center bg-gray-900 text-white gap-6">
-      <h1>
+      <p className="font-semibold text-[24px]">
         Toggle is: {isOn ? "On ✅" : "OFF ❌"}
-      </h1>
+      </p>
 
       <div
         onClick={() => setIsOn(!isOn)}
-        className={`w-16 h-8 flex items-center rounded-full p-1`}
+        className={`w-16 h-8 flex items-center rounded-full p-1 cursor-pointer transition-color duration-300 ${isOn ? "bg-green-500" : "bg-gray-500"}`}
       >
-
+        <div 
+          className={`bg-white w-6 h-6 rounded-full shadow-md transform transition-transform duration-300 ${isOn ? "translate-x-8" : "translate-x-0"}`}
+        >
+        </div>
       </div>
     </div>
   )
