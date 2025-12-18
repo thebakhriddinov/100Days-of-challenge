23977ef255a42cefbf78672230ac20dd801f0c95
Ozodbek <bakhriddinoff01@gmail.com>
2025-12-18 21:58:42 +0500

    initial commit

---
+++ Day31_Header_scroll_change/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+export default function App() {
+  const [scolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+        setScrolled(window,scrollY > 50);
+      };
+
+      window.addEventListener("scroll", handleScroll);
+      return () => window.removeEventListener("scroll", handleScroll); 
+  }, [])
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
