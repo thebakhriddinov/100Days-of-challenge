e97b85b9c2c09f8cd528c22aa71a0bec28338e6d
Ozodbek <bakhriddinoff01@gmail.com>
2025-12-19 22:35:25 +0500

    fixed code

--- Day32_Key_detector/src/App.jsx
+++ Day32_Key_detector/src/App.jsx
@@ -1,17 +1,16 @@
 import { useState, useEffect } from "react";
 
 export default function App() {
-
   const [keyData, setKeyData] = useState({
     key: "",
     code: "",
   });
 
-  useEffecgt(() => {
+  useEffect(() => {
     const handleKeyDown = (e) => {
       setKeyData({
         key: e.key === " " ? "Space" : e.key,
-        code: e. keyCode,
+        code: e.keyCode,
       });
     };
 
@@ -25,10 +24,15 @@ export default function App() {
       <h1 className="text-3xl font-bold mb-4">Key Detector</h1>
 
       {keyData.key ? (
-        <div className="bg-gray-">
-
+        <div className="bg-gray-800 p-6 rounded-2xl shadow-lg text-center space-y-4">
+          <div className="text-6xl font-extrabold text-blue-400">
+            {keyData.key}
+          </div>
+          <div className="text-xl text-gray-300">Key Code: {keyData.code}</div>
         </div>
+      ) : (
+        <p className="text-gray-400 text-lg">Tugmalardan birini bosing</p>
       )}
     </div>
-  )
+  );
 }
